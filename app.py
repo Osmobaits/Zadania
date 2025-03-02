179d84e7a3e356b5cc7b1baacee32577fd6d47be
Osmobaits <radoslawkrawczuk@gmail.com>
2025-03-02 23:20:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,50 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_restful import Api
 from extensions import db, ma
-from resources import TaskListResource, TaskResource, UserListResource  # Importuj zasoby
+from resources import TaskListResource, TaskResource, UserListResource
 from config import Config
-from models import Task, User  # Importuj modele
-
+from models import Task, User
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Inicjalizacja rozszerzeń
     db.init_app(app)
     ma.init_app(app)
 
-    # Inicjalizacja API
     api = Api(app)
-    api.add_resource(TaskListResource, '/api/tasks')  # Dodajemy prefix /api
+    api.add_resource(TaskListResource, '/api/tasks')
     api.add_resource(TaskResource, '/api/tasks/<int:task_id>')
     api.add_resource(UserListResource, '/api/users')
 
-
-    # Trasy dla widoków (renderowanie szablonów)
     @app.route('/')
     def index():
-        #Pobierz wszystkich użytkowników, aby przekazać do szablonu
-        # To jest potrzebne dla kolorów kafelków, nawet jeśli nie ma jeszcze API
-        users = User.query.all()
-        return render_template('task_list.html', users=users) #Przekazanie users
+        users = User.query.all() #Pobierz dla kolorów
+        return render_template('task_list.html', users=users)
 
-    @app.route('/add_user', methods=['POST']) #Tymczasowy endpoint do dodania usera
+    @app.route('/add_user', methods=['POST'])
     def add_user():
-        from flask import request # Importuj request wewnątrz funkcji
+        from flask import request
         username = request.form.get('username')
         password = request.form.get('password')  # TODO: Hashowanie!
         color = request.form.get('color')
 
-        if username and password and color: #Prosta walidacja
+        if username and password and color:
             new_user = User(username=username, password=password, color=color)
             db.session.add(new_user)
             db.session.commit()
-
         return redirect(url_for('index'))
 
-    # Utwórz tabele bazy danych (tylko raz, przy pierwszym uruchomieniu)
+
     with app.app_context():
         db.create_all()
 
     return app
 
+# --- ZMIANY TUTAJ ---
+app = create_app()  # Przypisz instancję aplikacji do zmiennej 'app'
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)  # Uruchom w trybie debugowania (tylko do developmentu!)
+    # NIE URUCHAMIAJ TEGO NA PRODUKCJI (OnRender)!
+    # app.run(debug=True)
+    pass #Zostaw puste, OnRender sam uruchomi
