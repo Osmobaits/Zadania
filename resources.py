7eaed7e05937e5973398e58590a5c935dfa32c4c
Osmobaits <radoslawkrawczuk@gmail.com>
2025-03-02 23:13:20 +0100

    Update resources.py

--- resources.py
+++ resources.py
@@ -10,10 +10,8 @@ tasks_schema = TaskSchema(many=True)
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
-
 class TaskListResource(Resource):
     def get(self):
-        # Na razie pobieramy wszystkie zadania (bez archiwizacji)
         tasks = Task.query.all()
         return tasks_schema.dump(tasks)
 
@@ -21,12 +19,11 @@ class TaskListResource(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('title', type=str, required=True, help='Title is required')
         parser.add_argument('description', type=str)
-        parser.add_argument('due_date', type=str)  # Przyjmujemy jako string ISO 8601
+        parser.add_argument('due_date', type=str)
         parser.add_argument('priority', type=int)
         parser.add_argument('assigned_to_id', type=int, required=True, help='Assigned user is required')
         args = parser.parse_args()
 
-        # Konwersja due_date (string -> datetime)
         if args['due_date']:
             try:
                 args['due_date'] = datetime.fromisoformat(args['due_date'].replace('Z', '+00:00'))
@@ -37,7 +34,6 @@ class TaskListResource(Resource):
         db.session.add(new_task)
         db.session.commit()
         return task_schema.dump(new_task), 201
-
 
 class TaskResource(Resource):
     def get(self, task_id):
@@ -52,10 +48,8 @@ class TaskResource(Resource):
         parser.add_argument('due_date', type=str)
         parser.add_argument('priority', type=int)
         parser.add_argument('assigned_to_id', type=int)
-        # completed, archived, in_progress - dodamy później
         args = parser.parse_args()
 
-        # Konwersja due_date
         if args['due_date']:
             try:
                 args['due_date'] = datetime.fromisoformat(args['due_date'].replace('Z', '+00:00'))
@@ -75,8 +69,7 @@ class TaskResource(Resource):
         db.session.commit()
         return '', 204
 
-
-class UserListResource(Resource):
+class UserListResource(Resource): #Potrzebne do pobierania listy userów przez API
     def get(self):
         users = User.query.all()
         return users_schema.dump(users)
