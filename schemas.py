e1cbc899c76bd358caa5002bf120ba0b01a2408f
Osmobaits <radoslawkrawczuk@gmail.com>
2025-03-02 22:58:39 +0100

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,1 +1,20 @@
+from marshmallow import Schema, fields, validate
+from extensions import ma
+from models import User, Task
 
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        load_instance = True
+        exclude = ('password',)  # Kluczowe: Nie wysyłaj hasła!
+        include_fk = True
+
+class TaskSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Task
+        load_instance = True
+        include_fk = True
+
+    assigned_to = ma.Nested(UserSchema, only=('id', 'username', 'color'))
+    due_date = fields.DateTime(format='%Y-%m-%dT%H:%M:%S')
+    created_at = fields.DateTime(format="%Y-%m-%dT%H:%M:%S", dump_only=True) #Tylko do odczytu
