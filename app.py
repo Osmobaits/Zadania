f3e1303077eba3b34a268f598b7460936097a655
Osmobaits <radoslawkrawczuk@gmail.com>
2025-03-03 19:08:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_restful import Api
-from extensions import db, ma
+from extensions import db, ma, login_manager  # Importuj login_manager
 from resources import TaskListResource, TaskResource, UserListResource
 from config import Config
 from models import Task, User
+from flask_migrate import Migrate
+from flask_login import login_user, logout_user, login_required, current_user # Import
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -11,40 +13,79 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     ma.init_app(app)
+    migrate = Migrate(app, db)
+    login_manager.init_app(app) # Inicjalizacja LoginManagera
 
     api = Api(app)
     api.add_resource(TaskListResource, '/api/tasks')
     api.add_resource(TaskResource, '/api/tasks/<int:task_id>')
     api.add_resource(UserListResource, '/api/users')
 
+    @login_manager.user_loader  # Dekorator Flask-Login
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     @app.route('/')
+    @login_required  # Wymagaj zalogowania
     def index():
-        users = User.query.all() #Pobierz dla kolorów
-        return render_template('task_list.html', users=users)
+        users = User.query.all()
+        return render_template('task_list.html', users=users, current_user=current_user) #Przekazanie current_user
 
-    @app.route('/add_user', methods=['POST'])
-    def add_user():
-        from flask import request
-        username = request.form.get('username')
-        password = request.form.get('password')  # TODO: Hashowanie!
-        color = request.form.get('color')
 
-        if username and password and color:
-            new_user = User(username=username, password=password, color=color)
-            db.session.add(new_user)
-            db.session.commit()
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+
+        if request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+            user = User.query.filter_by(username=username).first()
+
+            if user and user.check_password(password):
+                login_user(user)  # Zaloguj użytkownika
+                next_page = request.args.get('next') # Pobierz adres URL, do którego użytkownik próbował wejść
+                return redirect(next_page or url_for('index')) # Przekieruj na stronę główną lub 'next'
+            else:
+                flash('Login Unsuccessful. Please check username and password', 'danger') # Komunikat, kategoria bootstrap
+        return render_template('login.html')
+
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()  # Wyloguj użytkownika
         return redirect(url_for('index'))
 
+    @app.route('/register', methods=['GET', 'POST']) #Rejestracja użytkownika
+    def register():
+        if current_user.is_authenticated: #Jeśli zalogowany przekieruj
+            return redirect(url_for('index'))
 
-    with app.app_context():
-        db.create_all()
+        if request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+            color = request.form.get('color')
+
+            #Walidacja
+            if not username or not password or not color:
+                flash('Please fill out all fields', 'danger')
+                return render_template('register.html')
+
+            if User.query.filter_by(username=username).first():
+                flash('Username already exists', 'danger')
+                return render_template('register.html')
+
+            # Tworzenie i zapis usera
+            new_user = User(username=username, color=color)
+            new_user.set_password(password) # Haszowanie hasła
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash('Registration successful! You can now log in.', 'success')
+            return redirect(url_for('login')) #Przekierowanie do logowania
+
+        return render_template('register.html') #Wyświetlenie formularza
 
     return app
 
-# --- ZMIANY TUTAJ ---
-app = create_app()  # Przypisz instancję aplikacji do zmiennej 'app'
-
-if __name__ == '__main__':
-    # NIE URUCHAMIAJ TEGO NA PRODUKCJI (OnRender)!
-    # app.run(debug=True)
-    pass #Zostaw puste, OnRender sam uruchomi
+app = create_app()
