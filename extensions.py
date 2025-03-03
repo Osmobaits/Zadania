727c8f58af90b66893f9404fd6e80e8093de26c5
Osmobaits <radoslawkrawczuk@gmail.com>
2025-03-03 19:08:21 +0100

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_login import LoginManager  # Importuj LoginManager
 
 db = SQLAlchemy()
 ma = Marshmallow()
+login_manager = LoginManager()  # Dodaj LoginManager
+login_manager.login_view = 'login'  # Ustaw widok logowania (adres URL)
+login_manager.login_message_category = 'info' # kategoria (Bootstrap)
